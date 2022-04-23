546ed360f7aed2c11a33f95333c1061c687d1d2f
amv1017 <Vinokurov@bbs-c.ru>
2022-04-23 22:21:11 +0300

    matcher methods

--- sum.test.js
+++ sum.test.js
@@ -14,3 +14,68 @@ describe("example tests", () => {
   
 })
 
+describe("real or fake", () => {
+
+  it("null", () => {
+    const n = null
+    // expect(n).toBeFalsy()
+    // expect(n).not.toBeTruthy()
+    expect(n).toBeNull()
+  })
+
+})
+
+describe("numbers", () => {
+
+  it("nine plus nine", () => {
+    const value = 9 + 9
+    expect(value).toBeGreaterThan(15)
+    expect(value).toBeLessThanOrEqual(18)
+  })
+
+  it("adding floats", () => {
+    const value = 0.1 + 0.2
+    expect(value).toBeCloseTo(0.3)
+  })
+
+})
+
+describe("strings", () => {
+
+  it("there is no I in team", () => {
+    expect("team").not.toMatch(/I/)
+  })
+
+})
+
+describe("arrays", () => {
+
+  it("element in array", () => {
+    const shoppingList = ['toothpaste', 'soap', 'nuts', 'olive oil', 'sunflower seeds']
+    expect(shoppingList).toContain('olive oil')
+  })
+
+})
+
+function compileAndroidCode() {
+  throw new Error("You are using wrong JDK")
+}
+
+describe("exceptions", () => {
+
+  it("compiling android goes as expected", () => {
+    expect(() => compileAndroidCode()).toThrow("You are using wrong JDK")
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
